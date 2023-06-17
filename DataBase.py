05507fbe653678a6af6d00a7fe25458bcf7fc7e2
Gudipati Vishnu Vardhan Reddy <70902772+Uninterestedguy@users.noreply.github.com>
2023-06-17 00:04:49 +0000

    Adding Mongo Database

---
+++ DataBase.py
@@ -0,0 +1,11 @@
+import pymongo
+
+# UniteBot Cluster
+cluster = pymongo.MongoClient("mongodb+srv://Vishan:harsha@unitebot.jmii40m.mongodb.net/?retryWrites=true&w=majority")
+
+# UniteBot Database
+database = cluster['UniteBot']
+
+# Collections
+communities = database["Communities"]
+events = database["Events"]
