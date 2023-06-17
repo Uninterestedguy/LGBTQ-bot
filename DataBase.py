92614e7dd86db7f920bd52ec336aa5a70c7d21f3
Gudipati Vishnu Vardhan Reddy <70902772+Uninterestedguy@users.noreply.github.com>
2023-06-17 09:22:38 +0000

    Updating database.py

--- DataBase.py
+++ DataBase.py
@@ -16,20 +16,20 @@ distinctvalues = events.distinct("description")
 def gender_query(gender):
     db.Genders.find({'Gender':gender})
 
-def comm_query(Location=location,Community=community):
+def comm_query(Location,Community):
     db.Communities.find({'City of Organization/Program':Location,'Name of Organization/Program':Community})
 
-def comm_query(Location=location):
+def comm_query(Location):
     db.Communities.find({'City of Organization/Program':Location})
 
-def comm_query(Community=community):
+def comm_query(Community):
     db.Communities.find({'Name of Organization/Program':Community})
 
-def event_query(Location=location,Event=event):
-    db.Events.find({'description':Event,'location':Location'})
+def event_query(Location,Event):
+    db.Events.find({'description':Event,'location':Location})
 
-def event_query(Location=location):
-    db.Events.find({'location':Location'})
+def event_query(Location):
+    db.Events.find({'location':Location})
 
-def event_query(Event=event):
+def event_query(Event):
     db.Events.find({'description':Event})
