c25e919ef8476d425c948e154ba5a266f1448faa
Gudipati Vishnu Vardhan Reddy <70902772+Uninterestedguy@users.noreply.github.com>
2023-06-17 09:08:48 +0000

    Updating database variable

--- DataBase.py
+++ DataBase.py
@@ -4,12 +4,12 @@ import pymongo
 cluster = pymongo.MongoClient("mongodb+srv://Vishan:harsha@unitebot.jmii40m.mongodb.net/?retryWrites=true&w=majority")
 
 # UniteBot Database
-database = cluster['UniteBot']
+db = cluster['UniteBot']
 
 # Collections i.e tables
-communities = database["Communities"]
-events = database["Events"]
-genders= database["Genders"]
+communities = db["Communities"]
+events = db["Events"]
+genders= db["Genders"]
 
 distinctvalues = events.distinct("description")
 
